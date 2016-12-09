9fea705f2226b50e197f545a4ef3a9fa936845a1
Fadi Asfour <fadi@asfourconsulting.com>
2016-12-09 12:46:20 -0800

    added bootstrap sass & js

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,14 @@
 class UserController < ApplicationController
+  def new
+  end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to controller: 'system', action: 'index'
+    else
+      redirect_to action: 'new'
+    end
+  end
 end
