2803bce75bbda13c1f9819f727cbf835116b9819
Fadi Asfour <fadi@asfourconsulting.com>
2016-12-09 20:53:18 -0800

    complete authentication

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,23 @@
-require 'rails_helper'
+describe SessionsController, type: :controller do
+  describe 'GET' do
+    it 'returns http success for /login' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    
+    it 'returns http redirect for /logout' do
+      get :destroy
+      expect(response).to have_http_status(:redirect)
+    end
+  end
 
-RSpec.describe SessionsController, type: :controller do
-
+  describe 'POST' do
+    before (:example) do
+      User.create!(name: 'testy', email: 'best@test.com', password: 'besttest')
+    end
+    it 'return http redirect for login into system' do
+      post :create, params: { email: 'best@test.com', password: 'besttest' }
+      expect(response).to have_http_status(:redirect)
+    end
+  end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+describe UserController, type: :controller do
+  describe 'GET' do
+    it 'return http success for /register' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-RSpec.describe UserController, type: :controller do
-
+  describe 'POST' do
+    it 'returns http redirect for submitting registration' do
+      post :create, params: { user: {
+        name: 'testyb', 
+        email: 'testy@best.ca', 
+        password: 'testyb123', 
+        password_confirmation: 'testyb123' 
+        }
+      }
+      expect(response).to redirect_to(root_url)
+    end
+  end
 end
