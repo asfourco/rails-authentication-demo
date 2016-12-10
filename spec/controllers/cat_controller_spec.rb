bfcc1641aa7dd8804cc961815fb3661501726d57
Fadi Asfour <fadi@asfourconsulting.com>
2016-12-10 00:02:56 -0800

    gemfile cleanup

--- spec/controllers/cat_controller_spec.rb
+++ spec/controllers/cat_controller_spec.rb
@@ -4,10 +4,5 @@ describe CatController, type: :controller do
       get :show
       expect(response).to have_http_status(:redirect)
     end
-    
-    it 'is not valid for /cats' do
-      get '/cats'
-      expect(response).to have_http_status(:redirect)
-    end
   end
 end
