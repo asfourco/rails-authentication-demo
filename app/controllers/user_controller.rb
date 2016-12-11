0f054155703ef6732bce03a335a3b83f897a46d2
Fadi Asfour <fadi@asfourconsulting.com>
2016-12-11 15:34:30 -0800

    added signup form feedback, email verification, and reset password functionalities

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,18 +1,31 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      flash[:success] = 'Registration successful'
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+      flash[:success] = 'Please confirm your email address to continue'
       redirect_to root_path
     else
-      flash[:danger] = 'Error in user registration form, please try again'
-      redirect_to action: 'new'
+      render 'new'
     end
   end
   
+  def confirm_email
+    user = User.find_by(confirm_token: params[:token])
+    if user
+      user.email_activate
+      flash[:success] = 'Your email has been confirmed, please login to continue'
+      redirect_to login_path
+    else
+      flash[:danger] = 'Sorry, this email is not registered'
+      redirect_to root_path
+    end
+  end
+
   private
 
     def user_params
